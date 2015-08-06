9a7d7b4ba3b73cafd29a59a2d83b7734440bed63
syang019 <stanley@stanleycyang.com>
2015-08-05 21:13:12 -0700

    Added modules

---
+++ main.js
@@ -0,0 +1,7 @@
+var express = require('express'),
+    mongoose = require('mongoose'),
+    http = require('http'),
+    path = require('path'),
+    logger = require('morgan'),
+    bodyParser = require('body-parser'),
+    methodOverride = require('method-override');
